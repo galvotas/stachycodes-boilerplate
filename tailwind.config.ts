aa4e32427848ef93b5696d9446344bddeba5320a
Antanas Andrulis <antanas.andrulis@kilo.health>
2024-08-05 11:35:53 +0300

    initial boilerplate commit

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -8,10 +8,10 @@ const config: Config = {
   ],
   theme: {
     extend: {
-      backgroundImage: {
-        "gradient-radial": "radial-gradient(var(--tw-gradient-stops))",
-        "gradient-conic":
-          "conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))",
+      colors: {
+        dark: "#14171a",
+        green: "#43de78",
+        lightGray: "#f5f6fc",
       },
     },
   },
